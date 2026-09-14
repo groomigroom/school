a31d75daf94b3e084b0fb89e4cbc36d302e347fe
groomigroom <googjohn0143@gmail.com>
2026-09-14 12:04:52 +0900

    Document file path and extension details in path.js
    Add comments detailing file path information.

--- 웹사이트구축실습/4주차/path.js
+++ 웹사이트구축실습/4주차/path.js
@@ -14,3 +14,12 @@ const ext = path.extname(__filename);
 console.log(`파일 확장자: ${ext}`);
 console.log(`파일 확장자 제외 ${path.basename(__filename, ext)}`);
 
+/*
+파일 절대 경로: E:\황지원\ai웹사이트구축실습\server\path.js
+경로만: E:\황지원\ai웹사이트구축실습\server
+파일 이름 : path.js
+파일 이름 확장자 제외 : path
+파일 확장자: .js
+파일 확장자 제외 path
+*/
+
