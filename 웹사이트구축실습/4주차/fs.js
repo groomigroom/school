074198f6339963830701cc3d1182a62f912e35ec
groomigroom <googjohn0143@gmail.com>
2026-09-14 12:23:32 +0900

    Create fs.js

---
+++ 웹사이트구축실습/4주차/fs.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+// fs.readFile("example.txt", (err, data) => {
+//     console.log(data);
+// });
+
+fs.readdir("./", (err, files) => {
+    if (err) {
+        console.error(err);
+    }
+    console.log(files);
+});
+
+/*
+[
+  '1',             'example.txt',
+  'fs.js',         'function.js',
+  'greeting.js',   'hello.js',
+  'hello_1.js',    'httpServer.js',
+  'node_modules',  'package-lock.json',
+  'package.json',  'path.js',
+  'path_parse.js', 'user.js',
+  'user4.js',      'web_2.js'
+]
+*/
