7363de8851080636f1bee7eb9ac5d71f98fcee49
groomigroom <googjohn0143@gmail.com>
2026-09-14 14:17:40 +0900

    Update writefile.js

--- 웹사이트구축실습/4주차/writefile.js
+++ 웹사이트구축실습/4주차/writefile.js
@@ -2,3 +2,10 @@ const fs = require("fs");
 
 const data = fs.readFileSync("example.txt", "utf8");
 fs.writeFileSync("text1.txt", data);
+//파일 생성됨
+
+if (fs.existsSync("text1.txt")) {
+    console.log("file already exist");
+} else {
+
+}
