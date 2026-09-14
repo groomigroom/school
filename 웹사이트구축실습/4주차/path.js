d90f26c86269ed89e3b53d77d73db0a6bf672abf
groomigroom <googjohn0143@gmail.com>
2026-09-14 12:02:12 +0900

    Update path.js

--- 웹사이트구축실습/4주차/path.js
+++ 웹사이트구축실습/4주차/path.js
@@ -1,13 +1,17 @@
 const path = require("path");
 
 console.log(`파일 절대 경로: ${__filename}`);
-//파일 절대 경로: E:\황지원\ai웹사이트구축실습\server\path.js
 
 const dir = path.dirname(__filename);
 console.log(`경로만: ${dir}`);
-//경로만: E:\황지원\ai웹사이트구축실습\server
+
+const fn = path.basename(__filename);
+const fn2 = path.basename(__filename, ".js");
+console.log(`파일 이름 : ${fn}`);
+console.log(`파일 이름 확장자 제외 : ${fn2}`);
 
 // const ext = path.extname(__filename);
 // //확장자 이름
 // console.log(`파일 확장자: ${ext}`);
 // console.log(path.basename(__filename.ext));
+//파일 이름만 추출하는게 basename
