2d22ad5668434ba37ea1e9aee061f613d65f13d1
groomigroom <googjohn0143@gmail.com>
2026-09-14 14:24:42 +0900

    Update writefile.js

--- 웹사이트구축실습/4주차/writefile.js
+++ 웹사이트구축실습/4주차/writefile.js
@@ -1,11 +1,27 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("example.txt", "utf8");
-fs.writeFileSync("text1.txt", data);
+//const data = fs.readFileSync("example.txt", "utf8");
+// fs.writeFileSync("text1.txt", data);
 //파일 생성됨
 
+const data = fs.readFileSync("example.txt", "utf8", (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+
+});
+
+fs.writeFile("./text2.txt", data, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log("text2.txt is saved!");
+});
+
+/*
 if (fs.existsSync("text1.txt")) {
     console.log("file already exist");
 } else {
 
 }
+*/
