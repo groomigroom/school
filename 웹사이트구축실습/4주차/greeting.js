4ee657ab2dca9e898081c3c11a3981a42ff5bd5f
groomigroom <googjohn0143@gmail.com>
2026-09-14 11:24:15 +0900

    Create greeting.js

---
+++ 웹사이트구축실습/4주차/greeting.js
@@ -0,0 +1,16 @@
+const groomi = require('./user');
+const hello = require('./hello');
+const userss = require('./web_2');
+const user4s = require("./user4");
+
+const { user1, user2 } = require('./web_2');
+
+hello(groomi);
+hello(userss.user1);
+hello(userss.user2);
+hello(userss.user3);
+hello(user1);
+hello(user2);
+hello(user4s.user1);
+hello(user4s.user2);
+hello(user4s.user3);
