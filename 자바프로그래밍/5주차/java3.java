14c97d7f9029fa2d61f13b5209349800116bf353
groomigroom <googjohn0143@gmail.com>
2026-09-17 10:22:03 +0900

    Create java3.java

---
+++ 자바프로그래밍/5주차/java3.java
@@ -0,0 +1,15 @@
+package nine17pro1;
+
+public class HardProblem {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			int j = 0;
+			while(j < 10) {
+				j++;
+				
+			}
+		}
+	}
+
+}
