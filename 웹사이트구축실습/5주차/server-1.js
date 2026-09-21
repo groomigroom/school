0cfd2ec41b098100ed42c4e366622cad0120607b
groomigroom <googjohn0143@gmail.com>
2026-09-21 11:32:48 +0900

    Update server-1.js

--- 웹사이트구축실습/5주차/server-1.js
+++ 웹사이트구축실습/5주차/server-1.js
@@ -1,1 +1,9 @@
-server-1.js
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log("요청");
+});
+
+server.listen(3000, () => {
+    console.log("300번에서 http://localhost:3000");
+});
